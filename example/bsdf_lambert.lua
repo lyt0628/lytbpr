52112f411e56a5b72de6c9e4f3840eab7ff4e8f8
lyt0628 <LYT.0628@qq.com>
2024-12-26 11:58:30 +0800

    Add cache for Vec and Mat

--- example/bsdf_lambert.lua
+++ example/bsdf_lambert.lua
@@ -53,7 +53,7 @@ function test_ray_triangle_intersection(P, d, A, B, C)
       end
 end
 
-local img = PPM.new(64, 64)
+local img = PPM.new(512, 512)
 
 -- Light And Its Color
 local L = Vec.new(0, 0, 0)
@@ -63,31 +63,31 @@ local L_c = Vec.new(255, 200, 100)
 local lambert_factor = Vec.new(36, 36, 36)
 
 local P = Vec.new(-1, 0, 0)
-local A = Vec.new(1, -32, 31)
-local B = Vec.new(6, 1, 5)
-local C = Vec.new(1, 24, 20)
+local A = Vec.new(10, -1700, 1550)
+local B = Vec.new(60, 250, 500)
+local C = Vec.new(10, 1200, 1000)
 
 directions = {}
-for z = -32, 31 do
-  for y = -32, 31 do
+for z = -256, 255 do
+  for y = -256, 255 do
      table.insert(directions, Vec.new(1, y, z))
   end
 end
-for r = 1, 64 do
-  for c = 1, 64 do
-     local inside, Q, N = test_ray_triangle_intersection(P, directions[r + (c-1)*64 ],A,B,C)
+for r = 1, 512 do
+  for c = 1, 512 do
+     local inside, Q, N = test_ray_triangle_intersection(P, directions[r + (c-1)*512 ], A, B, C)
      if (inside) then
          local distance_to_light = #(Q-L)
          print("Distance:" .. distance_to_light)
-         local L_i = L_c / ( distance_to_light^2) * 15
+         local L_i = L_c / ( distance_to_light^2) 
          print("L_i:" .. L_i:r(),L_i:g(),L_i:b() )
          N = N:normlized()
         -- print("N: " .. N[1], N[2], N[3] ,#N)
          local D =  (Q-L):normlized()
         -- print("D: " .. D[1], D[2], D[3])
         -- print("Dot:" .. N:dot(D))
-         local L_o = L_i * lambert_factor * math.max(0, N:dot(D))
-        -- print("Output Color:" .. L_o[1],L_o[2],L_o[3] )
+         local L_o = L_i * lambert_factor * math.max(0, N:dot(D)) * 300000
+         print("L_o:" .. L_o[1],L_o[2],L_o[3] )
          img:set(r, c, L_o)
      end
   end
